024ebef892edc67ca71f350f9d5d2ed87a024ead
KetanMangukiya <ketan.mangukiya@bacancytechnology.com>
2016-12-30 12:15:44 +0530

    Remove Comment

--- range.rb
+++ range.rb
@@ -1,4 +1,4 @@
-digits = -1..9  
-puts digits.include?(5)          # true  
-puts digits.min                  # -1  
-puts digits.max                  # 9  
+digits = -1..9 
+puts digits.include?(5)
+puts digits.min
+puts digits.max
